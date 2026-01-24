4d21133c0e8501880bbbb8fbd0b131f813e3d483
Dilan Lakshithae <lakshithadilan6@gmail.com>
2026-01-24 13:30:19 +0530

    New language Changes use google translate

--- src/app/mainComponents/layout-component/layout-component.ts
+++ src/app/mainComponents/layout-component/layout-component.ts
@@ -4,14 +4,43 @@ import { ScrollToToComponent } from '../../sharedComponents/scroll-to-to-compone
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
-
 @Component({
   selector: 'app-layout-component',
   standalone: true,
-  imports: [CommonModule,ScrollToToComponent,RouterModule],
+  imports: [CommonModule, ScrollToToComponent, RouterModule],
   templateUrl: './layout-component.html',
-  styleUrl: './layout-component.css'
+  styleUrl: './layout-component.css',
 })
 export class LayoutComponent {
+  activeLang = 'en';
+  ngOnInit() {
+    const savedLang = localStorage.getItem('preferred_lang');
+    if (savedLang) {
+      setTimeout(() => {
+        const dropdown = document.getElementById(
+          'langSwitcher'
+        ) as HTMLSelectElement;
+        if (dropdown) dropdown.value = savedLang;
+      }, 500);
+    }
+  }
 
+  changeLang(lang: string) {
+    this.activeLang = lang;
+    localStorage.setItem('preferred_lang', lang);
+    this.applyGoogleTranslate(lang);
+  }
+
+  private applyGoogleTranslate(lang: string) {
+    const interval = setInterval(() => {
+      const select = document.querySelector(
+        '.goog-te-combo'
+      ) as HTMLSelectElement;
+      if (select) {
+        select.value = lang;
+        select.dispatchEvent(new Event('change'));
+        clearInterval(interval);
+      }
+    }, 300);
+  }
 }
